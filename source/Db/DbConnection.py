05a6c738dd247e409104fa9dacf2bf80188d1b39
maciej <maciejjbankowski@gmail.com>
2023-04-02 12:26:58 +0200

    Add db connection module

---
+++ source/Db/DbConnection.py
@@ -0,0 +1,59 @@
+from typing import Any, List
+from collections import defaultdict
+import pyodbc 
+
+def create_connection_string(driver: str, server: str, database: str):
+    return "Driver={" + driver + "};" + f"Server={server};" + f"Database={database};"+ "Trusted_Connection=yes;"
+
+DRIVER = "ODBC Driver 17 for SQL Server"
+SERVER = r"(localdb)\ProjectsV13"
+DATABASE = "Test"    
+
+class Singleton:
+    _instance = None 
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(Singleton, cls).__new__(cls)
+        return cls._instance
+
+
+class DBConnection(Singleton):
+
+    def __init__(self, connection_string: str) -> None:
+        self.connection_string = connection_string
+
+    def _connect(self) -> None:
+         self.conn = pyodbc.connect(self.connection_string)
+
+    def execute(self, sql: str, get_results: bool=False):
+        cursor = self.conn.cursor()
+        cursor.execute(sql)
+        if get_results:
+            return cursor.fetchall()
+        
+    def execute_many(self, sql: str, values: List[Any]):
+        cursor = self.conn.cursor()
+        cursor.fast_executemany = True
+        cursor.executemany(sql, values)
+
+    def __enter__(self):
+        self._connect()
+
+    def __exit__(self, exception_type, exception_value, exception_traceback):
+        self.conn.commit()
+        self.conn.close()
+
+    def select_as_dict(self, sql: str) -> dict:
+        result = defaultdict(list)
+        query_result = self.execute(sql, get_results=True)
+        try:
+            fields = [field[0] for field in query_result[0].cursor_description]
+            for values in query_result:
+                for field, value in zip(fields, values):
+                    result[field].append(value)
+            return result
+        except IndexError:
+            return dict()
+        
+conn = DBConnection(create_connection_string(DRIVER, SERVER, DATABASE))
