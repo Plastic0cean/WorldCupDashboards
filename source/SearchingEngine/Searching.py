1415189c081bf97b5d1e35b58b1f96d67385ae04
maciej <maciejjbankowski@gmail.com>
2023-06-20 20:40:40 +0200

    format code in fuzzy matching class

--- source/SearchingEngine/Searching.py
+++ source/SearchingEngine/Searching.py
@@ -1,11 +1,18 @@
 from .FuzzyStringsMatcher import FuzzyStringsMatcher
+from Config import config
 
-matcher = FuzzyStringsMatcher(0.8)
+class PlayersSeaching:
 
-def fuzzy_filter_players(players_list: list, searched_string: str) -> list:
-    result = []
-    for player in players_list:
-        if matcher.contains_substring(player.given_name, searched_string) or matcher.contains_substring(player.family_name, searched_string):
-            result.append(player)
-    return result
+    def __init__(self, engine) -> None:
+        self.engine = engine
+
+    def search(players_list: list, searched_string: str) -> list:
+        result = []
+        for player in players_list:
+            if engine.contains_substring(player.given_name, searched_string) or engine.contains_substring(player.family_name, searched_string):
+                result.append(player)
+        return result
         
+
+engine = FuzzyStringsMatcher(config.searching_threshold)
+players_searching = PlayersSeaching(engine)
