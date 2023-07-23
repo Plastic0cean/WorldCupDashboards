f9114ab9de88b9626ace7be6d2fe9925cf059ec7
maciej <maciejjbankowski@gmail.com>
2023-07-23 19:09:16 +0200

    migrate sql procedures to mysql

---
+++ db/MySQL/Procedures/Teams/PositionsOnTournaments.sql
@@ -0,0 +1,19 @@
+DELIMITER //
+
+CREATE PROCEDURE PositionsOnTournaments (teamid VARCHAR(10))
+BEGIN
+    SELECT 
+    	q.tournament_id, 
+        tournament_name(t.year, t.host_country) AS tournament_name,
+		q.count_matches, 
+        performance,
+        g.position AS group_position
+    FROM qualified_teams q 
+    JOIN tournaments t ON t.tournament_id = q.tournament_id
+    LEFT JOIN group_standings g 
+    ON g.tournament_id = q.tournament_id AND q.team_id = g.team_id
+	WHERE q.team_id = teamid;
+    
+END //
+DELIMITER ;     
+
