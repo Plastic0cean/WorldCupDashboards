ae4f95259381f5a97facffdc45f7c1400d77a74a
maciej <maciejjbankowski@gmail.com>
2023-08-11 09:47:24 +0200

    change repositories names

--- source/blueprints/teams.py
+++ source/blueprints/teams.py
@@ -1,39 +1,38 @@
 import os
 from flask import Blueprint, render_template, request, redirect, url_for, send_from_directory
-from Reports.teams import repository
+from repository.teams import team_repository, team_stats_repository
 from utils.utils import retry_if_fail
 import Visualizations.plots as vis
 
 teams = Blueprint('teams', __name__)
 
-def generate_data(team_id: str, repository):
+def generate_data(team_id: str):
     return {
-        "team": repository.get_by_id(team_id),
-        "goals_and_matches": repository.get_goals_and_matches_summary(team_id),
-        "biggest_win": repository.get_biggest_win(team_id),
-        "biggest_defeat": repository.get_biggest_defeat(team_id),
-        "top_scorers": repository.get_top_scorers(team_id, 10),
-        "position_by_tournament": repository.get_position_by_tournaments(team_id)
+        "team": team_repository.get_by_id(team_id),
+        "biggest_win": team_stats_repository.get_biggest_win(team_id),
+        "biggest_defeat": team_stats_repository.get_biggest_defeat(team_id),
+        "top_scorers": team_stats_repository.get_top_scorers(team_id, 10),
+        "position_by_tournament": team_stats_repository.get_position_by_tournaments(team_id)
     }
 
-def generate_visualizations(team_id, repository):
+def generate_visualizations(team_id):
     return {
-        "matches_results": vis.results_of_matches_pie(repository.get_matches_results(team_id)),
-        "goals_by_opponent": vis.team_goals_by_opponent(repository.get_goals_by_opponent(team_id)),
-        "goals_by_tournament": vis.team_goals_by_tournament(repository.get_goals_by_tournament(team_id)),
-        "most_minutes_by_player": vis.players_with_most_minutes(repository.get_minutes_played_by_players(team_id)),
-        "matches_chart": vis.all_matches_by_team(repository.get_list_of_matches(team_id))
+        "matches_results": vis.results_of_matches(team_stats_repository.get_results_summary(team_id)),
+        "goals_by_opponent": vis.team_goals_by_opponent(team_stats_repository.get_goals_by_opponent(team_id)),
+        "goals_by_tournament": vis.team_goals_by_tournament(team_stats_repository.get_goals_by_tournament(team_id)),
+        "most_minutes_by_player": vis.players_with_most_minutes(team_stats_repository.get_minutes_played_by_players(team_id)),
+        "matches_chart": vis.all_matches_by_team(team_stats_repository.get_list_of_matches(team_id))
     }
 
 @teams.route("/teams/<team_id>", methods=("GET", "POST"))
 def team_details(team_id: str):
-    data = generate_data(team_id, repository)
-    visualizations = generate_visualizations(team_id, repository)
+    data = generate_data(team_id)
+    visualizations = generate_visualizations(team_id)
     return render_template("team_detals.html", data=data, visualizations=visualizations)
 
 @teams.route("/teams", methods=("GET", "POST"))
 def teams_selection():
-    teams = repository.get_all()
+    teams = team_repository.get_all()
     if request.method == "POST":
         team_id = request.form["teams-dropdown"]
         return redirect(url_for("teams.team_details", team_id=team_id))
@@ -41,7 +40,7 @@ def teams_selection():
 
 @retry_if_fail
 def get_flag_filename(team_id: str):
-    filename = repository.get_by_id(team_id).flag_img
+    filename = team_repository.get_by_id(team_id).flag_img
     return filename
 
 @teams.route("/flags/<team_id>")
--- source/blueprints/tournaments.py
+++ source/blueprints/tournaments.py
@@ -1,29 +1,28 @@
 from flask import Blueprint, render_template, request
-from Reports.tournaments import repository
+from repository.tournaments import tournament_repository, tournament_stats_repository
 import Visualizations.plots as vis
 
 tournaments = Blueprint('tournaments', __name__)
 
-def generate_data(tournament_id: str, repository):
+def generate_data(tournament_id: str):
     return {
-        "tournament": repository.get_by_id(tournament_id),
-        "tournaments_list": repository.get_all(),
-        "most_goals_in_single_game": repository.get_most_goals_in_single_game(tournament_id),
-        "most_cards_in_single_game": repository.get_most_cards_in_single_game(tournament_id),
-        "top_scorers": repository.get_top_scorers(tournament_id=tournament_id, how_many=10)
+        "tournament": tournament_repository.get_by_id(tournament_id),
+        "tournaments_list": tournament_repository.get_all(),
+        "most_goals_in_single_game": tournament_stats_repository.get_most_goals_in_single_game(tournament_id),
+        "most_cards_in_single_game": tournament_stats_repository.get_most_cards_in_single_game(tournament_id),
+        "top_scorers": tournament_stats_repository.get_top_scorers(tournament_id=tournament_id, how_many=10)
     }
 
-def generate_visualizations(tournament_id: str, repository):
+def generate_visualizations(tournament_id: str):
     return {
-        "goals_by_tournament": vis.goals_by_tournament(repository.get_goals_and_games_by_tournament()),
-        "goals_by_minutes": vis.goals_by_minutes(repository.get_goals_by_minutes(tournament_id)),
-        "goals_difference": vis.goals_difference_by_team(repository.get_goals_difference_by_team(tournament_id))
+        "goals_by_minutes": vis.goals_by_minutes(tournament_stats_repository.get_goals_by_minutes(tournament_id)),
+        "goals_difference": vis.goals_difference_by_team(tournament_stats_repository.get_goals_difference_by_team(tournament_id))
     }
 
 
 @tournaments.route("/tournaments")
 def tournament_details():
     tournament_id = request.args.get('id', default=None)
-    data = generate_data(tournament_id, repository)
-    visualizations = generate_visualizations(tournament_id, repository)
+    data = generate_data(tournament_id)
+    visualizations = generate_visualizations(tournament_id)
     return render_template("tournaments.html", data=data, visualizations=visualizations)
