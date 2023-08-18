889a02a63d6febbac9796a707e6383f7e9798994
maciej <maciejjbankowski@gmail.com>
2023-08-18 10:53:56 +0200

    change biggest win and biggest defeat procedures to match scorecard schema. Replace old html code with macro

--- db/MySQL/Procedures/Teams/BiggestDefeat.sql
+++ db/MySQL/Procedures/Teams/BiggestDefeat.sql
@@ -1,17 +1,17 @@
-DELIMITER //
 CREATE PROCEDURE BiggestDefeat (teamid VARCHAR(10))
-BEGIN
 	SELECT 
+		a.match_id,
 		a.tournament_id, 
 		tour.tournament_name, 
-		a.opponent_id, 
-		t.team_name as opponent_name, 
+		m.home_team_id, 
+		m.away_team_id,
+		t.team_name AS home_team_name,
+		t2.team_name as away_name, 
 		m.match_date, 
 		score(a.goals_for, a.goals_against) as score
 	FROM team_appearances a
 	JOIN tournaments tour ON tour.tournament_id = a.tournament_id
-	JOIN teams t ON t.team_id = a.opponent_id
 	JOIN matches m ON a.match_id = m.match_id
+	JOIN teams t ON t.team_id = m.home_team_id
+	JOIN teams t2 ON t2.team_id = m.away_team_id
 	WHERE a.team_id = teamid AND a.lose=1 AND goals_against - goals_for  = (SELECT MAX(goals_against - goals_for) FROM team_appearances WHERE team_id = teamid AND Lose=1);
-END //
-DELIMITER ; 
