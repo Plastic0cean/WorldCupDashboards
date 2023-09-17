5f1f2324c80fe8f47e87dd8fc65ddd11a161499f
maciej <maciejjbankowski@gmail.com>
2023-09-17 14:54:00 +0200

    delete unused class "events collection"

--- source/entities/match_event.py
+++ source/entities/match_event.py
@@ -1,4 +1,3 @@
-from collections import defaultdict
 from dataclasses import dataclass
 
 
@@ -14,27 +13,3 @@ class MatchEvent:
     team_name: str
     home_team: bool
     type: str
-
-
-class EventsCollection:
-    # TODO: Check if it's used anywhere
-
-    def __init__(self, events: list[MatchEvent]=None) -> None:
-        if events:
-            self.events = events
-        else:
-            self.events = []
-
-    def add(self, event: MatchEvent) -> None:
-        self.events.append(event)
-
-    def to_dict(self):
-        def default_value():
-            return {"home_team": [], "away_team": []}
-        result_events = defaultdict(default_value)
-        for event in self.events:
-            if event.home_team == 1:
-                result_events[event.minute_label]["home_team"].append(event)
-            else:
-                result_events[event.minute_label]["away_team"].append(event)
-        return dict(result_events)
